418203893d5d55dcb66e6fa2bec6f72459358846
pablooliveiraceni <cenipablo16@gmail.com>
2021-01-12 13:26:52 -0300

    Pablo Ceni

---
+++ backend/Model/ClassTable/Usuario.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once("../Connection/ClassConnection.php");
+
+
+class Usuario
+{
+    public $id_usuario;
+    public $nome;
+    public $telefone;
+    public $email;
+    public $senha;
+    public $endereco;
+    public $nivel_acesso;
+
+    public static function getAll()
+    {
+        $connection = Connection::getDb();
+
+        $stmt = $connection->query("SELECT * FROM usuarios");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+    }
+
+    public function registerUsuario()
+    {
+        $connection = Connection::getDb();
+
+        $stmt = $connection->query("INSERT INTO usuarios(name, email, telefone, senha, endereco) values ('$this->nome', '$this->email', '$this->telefone', '$this->senha', '$this->endereco')");
+ 
+        //Verifica quantas linhas foram afetadas 
+        if ($stmt->rowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
