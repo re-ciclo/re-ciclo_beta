5e4b9e57bf3e39080f6c1ff4688ab6794269d0e1
Vanessa Teixeira <vanessateixeira516@gmail.com>
2021-01-18 12:02:53 -0300

    MOdificado imagem carrosseul e finalizado pagina projeto -v

--- frontend/src/pages/Projeto/index.jsx
+++ frontend/src/pages/Projeto/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import imagem1 from './img/img-1.png';
-import imagem2 from './img/img-2.png';
-import imagem3 from './img/img-3.png';
-import imagem4 from './img/img-5.jpg';
-import imagem5 from './img/img-5.png';
-import imagem6 from './img/img-6.png';
+import imagem1 from './img/1.png';
+import imagem2 from './img/2.png';
+import imagem3 from './img/3.png';
+import imagem4 from './img/4.png';
+import imagem5 from './img/5.png';
+import imagem6 from './img/6.png';
 import { Carousel, Cont, Image } from 'react-bootstrap';
-import Title from '../../components/Title';
+
 
 
 
@@ -23,40 +23,45 @@ const Projeto = () => {
                 <h1 className="ml-2"></h1>
             </div>
             <div className="row my-4">
-                <div className="col-12 col-sm-6 bg-white">
+                <div className="col-12 col-sm-6 bg-white text-center">
 
                <h2> Maracanã</h2>
+               <br>
+               </br>
 
-                    <p> A comunidade Maracanã, fica localizada na região Sul de São Paulo.
-                Rodeada de prédios populares.</p>
+                    <p class="text-sm-center"> A comunidade Maracanã, fica localizada 
+                    na região Sul de São Paulo.Essa comunidade sofria por não possuirem
+                    uma moradia digna e confortável.Hoje essa realizade mudou, esses moradores vivem
+                    em prédios populares, mas infelizmente sem uma infraestrura apropriada.
+                </p>
 
                 
                 
 
 
                 </div>
-                <div className="col-12 col-sm-4  bg-white">
+                <div className="col-12 col-sm-6 bg-white ">
                     <Carousel>
                         <Carousel.Item>
                             <img
-                                className="d-block w-100"
-                                src={imagem1} class="img-fluid"
+                                className="d-block w-50"
+                                src={imagem1} class="img-responsive"
                                 alt="First slide"
                             />
 
                         </Carousel.Item>
                         <Carousel.Item>
                             <img
-                                className="d-block w-100"
-                                src={imagem2} class="img-fluid"
+                                className="d-block w-50"
+                                src={imagem2} class="img-responsive"
                                 alt="Third slide"
                             />
 
                         </Carousel.Item>
                         <Carousel.Item>
                             <img
-                                className="d-block w-100"
-                                src={imagem3} class="img-fluid"
+                                className="d-block w-50"
+                                src={imagem3} class="img-responsive"
                                 alt="Third slide"
 
                             />
@@ -70,9 +75,16 @@ const Projeto = () => {
                 </div>
             </div>
             <div className="row my-4">
-                <div className="col-12 col-sm-6 bg-white">
+                <div className="col mx-auto text-center  bg-white">
+                    
 
-                    <Image src={imagem4} roundedCircle />
+                    <div className="d-block w-100">
+                    
+                    <img src={imagem6} class="img-responsive" />
+
+                </div>
+
+                    
 
 
 
@@ -81,17 +93,25 @@ const Projeto = () => {
 
 
 
-                <div className="col-12 col-sm-6 bg-white">
+                <div className="col-12 col-sm-6  bg-white">
+                    <h2> Nosso Objetivo</h2>
+                    <br>
+                    </br>
+                    <p class="text-sm-center">Reverter materias reciclados
+                    em benefícios para as comunidades.Conscientizando assim, 
+                    a importância de adotar estilos sustentáveis de vida.</p>
                
-                    <p> É reverter materias reciclados
-                    em beneficios para as comunidades.</p>
+                   
 
                 </div>
             </div>
             <div className="row my-4">
-                <div className="col-12 col-sm-6  bg-white">
+                <div className="col-12 col-sm-6">
+                    <h2> Resultados</h2>
+                    <br>
+                    </br>
                 
-                    <p>Com recursos convertidos pela a ação da reciclagem,
+                    <p class="text-sm-center">Com recursos convertidos pela a ação da reciclagem,
                      todos serão beneficiados</p>
                 </div>
                 
@@ -100,16 +120,16 @@ const Projeto = () => {
                     <Carousel>
                         <Carousel.Item>
                             <img
-                                className="d-block w-100"
-                                src={imagem5} class="img-fluid"
+                                className="d-block w-50"
+                                src={imagem5} class="img-responsive"
                                 alt="First slide"
                             />
 
                         </Carousel.Item>
                         <Carousel.Item>
                             <img
-                                className="d-block w-100"
-                                src={imagem6} class="img-fluid"
+                                className="d-block w-50"
+                                src={imagem4} class="img-responsive"
                                 alt="Third slide"
                             />
 
@@ -124,14 +144,10 @@ const Projeto = () => {
                 </div>
             </div>
 
-            <div className="row my-4">
-                <div className="col-12 col-sm-6 bg-success">Carrosell</div>
-                <div className="col-12 col-sm-6 bg-primary">
-                    
+            
                 </div>
                 
-            </div>
-        </div>
+        
     );
 }
 
